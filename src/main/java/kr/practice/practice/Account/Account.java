f7d0a2d4475a1e766ba6342ef94ff7f20fc0f969
minal <minala7870@gmail.com>
2022-03-17 12:56:22 +0900

    post test

--- src/main/java/kr/practice/practice/Account/Account.java
+++ src/main/java/kr/practice/practice/Account/Account.java
@@ -8,4 +8,22 @@ public class Account {
 
     @Id
     private Long AccountNUm;
+    private String name;
+
+    public Long getAccountNUm() {
+        return AccountNUm;
+    }
+
+    public void setAccountNUm(Long accountNUm) {
+        AccountNUm = accountNUm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/main/java/kr/practice/practice/controller/AccountApiController.java
+++ src/main/java/kr/practice/practice/controller/AccountApiController.java
@@ -4,10 +4,9 @@ import kr.practice.practice.Account.Account;
 import kr.practice.practice.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -19,10 +18,25 @@ public class AccountApiController {
     // 생성자 주입으로 AccountRepository를 주입시켜준다.
     private final AccountRepository accountRepository;
 
+    //생성
     @PostMapping("/account")
     public ResponseEntity<?> saveAccount(@RequestBody Account account){
         Account saveAccount = accountRepository.save(account);
+        System.out.println("결과: "+account);
         return ResponseEntity.ok(saveAccount);
+    }
+
+    //목록 조회
+    @GetMapping("/account")
+    public List<Account> all(){
+        return accountRepository.findAll();
+    }
+    // 단건 조회
+
+    @GetMapping("/account/{id}")
+    public Account getAccount(@PathVariable Long id){
+        return accountRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("illegal argument :" + id));
     }
 
 
