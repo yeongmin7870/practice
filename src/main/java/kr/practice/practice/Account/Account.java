6671955f3a79c7f17da12075387088944e5ee4a3
ymgood <minala7870@gmail.com>
2022-03-17 15:02:23 +0900

    test

--- src/main/java/kr/practice/practice/Account/Account.java
+++ src/main/java/kr/practice/practice/Account/Account.java
@@ -1,12 +1,15 @@
 package kr.practice.practice.Account;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Account {
 
     @Id
+    @GeneratedValue
     private Long AccountNUm;
     private String name;
 
@@ -26,4 +29,27 @@ public class Account {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "Account{" +
+                "AccountNUm=" + AccountNUm +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(AccountNUm, account.AccountNUm) && Objects.equals(name, account.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(AccountNUm, name);
+    }
+
+    public Account() {
+    }
 }
--- src/main/java/kr/practice/practice/controller/AccountApiController.java
+++ src/main/java/kr/practice/practice/controller/AccountApiController.java
@@ -2,53 +2,30 @@ package kr.practice.practice.controller;
 
 import kr.practice.practice.Account.Account;
 import kr.practice.practice.repository.AccountRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/api")
-@RequiredArgsConstructor
 public class AccountApiController {
 
 
     // 생성자 주입으로 AccountRepository를 주입시켜준다.
-    private final AccountRepository accountRepository;
+    private final AccountRepository repository;
 
-    //생성
-    @PostMapping("/account")
-    public Account saveAccount(@RequestBody Account account){
-        return accountRepository.save(account);
+    AccountApiController(AccountRepository repository){
+        this.repository = repository;
     }
 
-
-    @PutMapping("/{AccountNUm}")
-    public void updateAccount(@PathVariable Long AccountNUm, @RequestBody Account newAccount){
-        accountRepository.findById(AccountNUm)
-                .map(account -> {
-                    account.setName(newAccount.getName());
-                    return accountRepository.save(account);
-                })
-                .orElseGet(() -> {
-                    newAccount.setAccountNUm(AccountNUm);
-                    return accountRepository.save(newAccount);
-                });
-    }
-
-
-    //목록 조회
+    //전체 조회
     @GetMapping("/account")
-    public List<Account> all(){
-        return accountRepository.findAll();
+    List<Account> all() {
+        return repository.findAll();
     }
-    // 단건 조회
 
-    @GetMapping("/account/{id}")
-    public Account getAccount(@PathVariable Long id){
-        return accountRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("illegal argument :" + id));
+    @PostMapping("/account")
+    Account newAccount(@RequestBody Account newAccount){
+        return repository.save(newAccount);
     }
 
 
